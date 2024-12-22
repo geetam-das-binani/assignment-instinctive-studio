84efc8c971d0cac9d9cba2c31ade0de170215320
geetam <geetambinani6@gmail.com>
2024-12-22 15:02:08 +0530

    updated package json file

--- backend/index.mjs
+++ backend/index.mjs
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 import {userRoutes} from './routes/userRoute.mjs';
 import {postRoutes} from './routes/postRoute.mjs';
-
+import path from "path";
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(
@@ -19,9 +20,17 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 app.use('/api/v1',userRoutes);
 app.use('/api/v1',postRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+
+
+// cd frontend && npm install && npm run build  && cd ../backend && npm run build 
+
+
+// start command -> cd backend && npm start 
